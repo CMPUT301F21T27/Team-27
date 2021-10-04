4faeff45fa427e6222f59aa37292315b116295f4
lantian-zhao <64811288+lantian-zhao@users.noreply.github.com>
2021-10-04 14:16:40 -0600

    Update Shape.java

--- app/src/main/java/com/example/team27/Shape.java
+++ app/src/main/java/com/example/team27/Shape.java
@@ -5,7 +5,7 @@ public abstract class Shape {
     public Integer y;
     public String color = "$PUT_YOUR_CHOICE_OF_COLOR";
     public Shape(int x, int y) {
-
+    String color = "blue;
     }
 
 }
