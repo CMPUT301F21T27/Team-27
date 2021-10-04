bb6928f7f3de5597b87d2e17f78fba81ac2ec990
“Weitong <weitong@ualberta.ca>
2021-10-04 13:40:32 -0600

    edit

--- app/src/main/java/com/example/team27/Shape.java
+++ app/src/main/java/com/example/team27/Shape.java
@@ -1,11 +1,7 @@
 package com.example.team27;
 
 public abstract class Shape {
-    public Integer x;
-    public Integer y;
-    public String color = "blue";
-    public Shape(int x, int y) {
 
-    }
+    public String color = "blue";
 
 }
